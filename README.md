8d37cc368e6f3a7a56ed2878964d8a24225aa1e8
thegif <gif@163.com>
2019-12-30 14:59:21 +0800

    add comment

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-第一个长期维护小xiangmu
+ 
 
 #  myLog          base
 ## 自定义日志
-基于切面自定义日志
+# 基于spring Aop AspecJ 切面自定义日志
+# spring boot  +  tkMybatis
--- src/main/java/com/sys/log/LogsApplication.java
+++ src/main/java/com/sys/log/LogsApplication.java
@@ -1,10 +1,12 @@
 package com.sys.log;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableAdminServer
 @MapperScan(basePackages = {"com.sys.log.mapper"})
 public class LogsApplication {
 
